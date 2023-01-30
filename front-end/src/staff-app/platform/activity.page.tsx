c7eb23e3893c50a3cc4421c1583bafefc6f772c4
100ngramjit <sangramjitdutta@gmail.com>
2023-01-30 12:19:38 +0530

    table added in activity page

--- front-end/src/staff-app/platform/activity.page.tsx
+++ front-end/src/staff-app/platform/activity.page.tsx
@@ -1,21 +1,52 @@
 //external imports
-import React, { useEffect } from "react"
+import React, { useContext, useEffect } from "react"
 import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
 import { makeStyles } from "@material-ui/styles"
-import { Accordion, AccordionSummary, Typography, AccordionDetails, Divider, Chip } from "@material-ui/core"
+import {
+  Accordion,
+  AccordionSummary,
+  Typography,
+  AccordionDetails,
+  Divider,
+  Chip,
+  Container,
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableBody,
+  withStyles,
+} from "@material-ui/core"
 
 //internal
-import { FontSize, Spacing } from "shared/styles/styles"
+import { FontSize, FontWeight, Spacing } from "shared/styles/styles"
 import { useApi } from "shared/hooks/use-api"
 import { Activity } from "shared/models/activity"
 import { RolllStateType } from "shared/models/roll"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
+import { RollContext } from "shared/context/RollContext"
+import { PersonHelper } from "shared/models/person"
+import { Colors } from "shared/styles/colors"
 
 export const ActivityPage: React.FC = () => {
   //misc
   const [getRoll, RollData, RollLoadState] = useApi<{ activity: Activity[] }>({ url: "get-activities" })
+  const { data } = useContext(RollContext)
+
+  const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: `${Colors.blue.base}`,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell)
+
   const useStyles = makeStyles((theme) => ({
     root: {
       width: "100%",
@@ -86,20 +117,31 @@ export const ActivityPage: React.FC = () => {
                 <Typography className={classes.heading}>{entity.name}</Typography>
                 <Typography className={classes.secondaryHeading}>{getDate(date)}</Typography>
               </AccordionSummary>
-              <Divider />
-              <AccordionDetails className={classes.details}>
-                <Typography>Student ID</Typography>
-                <Typography>Roll State</Typography>
-              </AccordionDetails>
-              <Divider />
-              {entity.student_roll_states.map(({ student_id, roll_state }) => {
-                return (
-                  <AccordionDetails key={student_id.toString()} className={classes.details}>
-                    <Typography>{student_id}</Typography>
-                    <Chip label={roll_state} style={{ backgroundColor: getBgColor(roll_state) }} />
-                  </AccordionDetails>
-                )
-              })}
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <StyledTableCell align="left">Student ID</StyledTableCell>
+                      <StyledTableCell align="left">Student Name</StyledTableCell>
+                      <StyledTableCell align="left">Roll State</StyledTableCell>
+                    </TableRow>
+                  </TableHead>
+
+                  <TableBody>
+                    {entity.student_roll_states.map(({ student_id, roll_state }, index) => {
+                      return (
+                        <TableRow key={student_id.toString()} className={classes.details}>
+                          <TableCell align="left">{student_id}</TableCell>
+                          <TableCell align="left">{PersonHelper.getFullName(data?.students[index]!)}</TableCell>
+                          <TableCell align="left">
+                            <Chip label={roll_state} style={{ backgroundColor: getBgColor(roll_state) }} />
+                          </TableCell>
+                        </TableRow>
+                      )
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </Accordion>
           </div>
         ))}
